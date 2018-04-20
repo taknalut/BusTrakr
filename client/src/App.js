6c1df74ce62dcdfcf3122806272c44d2e69340b7
Wai Yan <wai.yan0011553@gmail.com>
2018-04-20 11:22:04 -0400

    add user to db function

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Home from "./pages";
 import Nav from "./components/Nav";
+import Form from "./components/Form";
 
 const App = () => (
   <div>
     // <Nav />
     <Home />
+    <Form />
   </div>
 );
 
--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.static("client/build"));
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/nytimesapp");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/busTrackr");
 
 // Start the API server
 app.listen(PORT, function() {
