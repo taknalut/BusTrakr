ab2b4b8ac989b9e4544166a07fae2fdc409a677a
Sean Andersen <smandersen13@gmail.com>
2018-04-21 15:20:00 -0400

    added stops

--- client/src/components/Map/Map.js
+++ client/src/components/Map/Map.js
@@ -1,8 +1,7 @@
 import React, {Component} from "react";
-import { compose, withProps, withState, withHandlers } from "recompose";
-import { withGoogleMap, GoogleMap, Marker, Polyline } from "react-google-maps";
-// const BusStopPic = require("../components/Images/bus-stop.png");
-// const BusPic = require("../components/Images/bus.png");
+import { compose, withProps, withStateHandlers} from "recompose";
+import { withGoogleMap, GoogleMap, Marker, Polyline, InfoWindow } from "react-google-maps";
+import Search from "../Search";
 
 
 const google = window.google;
@@ -15,46 +14,67 @@ const MapRender = compose(
     containerElement: <div style={{ height: `400px` }} />,
     mapElement: <div style={{ height: `100%` }} />,
   }),
-  // withState('zoom', 'onZoomChange', 8, 'pathBounds'),
-  withHandlers(() => {
-    const refs = {
-      map: undefined,
-    }
-    return {
-      onMapMounted: () => ref => {
-        refs.map = ref
-      },
-      onZoomChanged: ({ onZoomChange }) => () => {
-        onZoomChange(refs.map.getZoom())
-      }
-      // ,
-      // fitBounds: ({ pathBounds }) => () => {
-      //   pathBounds(refs.map.fitBounds(Polyline.getBounds()))
-      //   console.log("working?")
-
-      // {map.fitBounds({props.test2}.getBounds())}
-      // }
-    }
+  withStateHandlers(() => ({
+    isOpen: false,
+    locationClicked: null
+  }), {
+    handleToggleOpen(item) {
+  		this.setState({
+  			position : {
+  				lat : item.lat,
+  				lng : item.lng
+  			}
+  		})
+  	},
+    onToggleOpen: ({ isOpen }) => () => ({
+      isOpen: !isOpen,
+    })
+    // showInfo(a) {
+    //   this.setState({showInfoIndex: a })
+    // }
   }),
   withGoogleMap
 )(props =>
-  <GoogleMap
-    ref={props.onMapMounted}
-    defaultZoom={props.defaultZoom}
-    center={props.center}
-    defaultCenter={{ lat: 38.9072, lng: -77.0369 }} >
-  {props.markers.map((bus,i) => (
-    <Marker key={i} position={bus.position} animation={google.maps.Animation.DROP}
-      icon={{
-        url:"../Images/bus.png",
-        size: new google.maps.Size(300, 300),
-        origin: new google.maps.Point(0, 0),
-        anchor: new google.maps.Point(17, 34),
-        scaledSize: new google.maps.Size(25, 25)}}
-    />
-  ))}
-  <Polyline path={props.test} options={{strokeColor:'black',strokeWeight: 4}} />
-  </GoogleMap>
+    <GoogleMap
+      ref={props.onMapMounted}
+      defaultZoom={props.defaultZoom}
+      center={props.center}
+      defaultCenter={{ lat: 38.9072, lng: -77.0369 }} >
+    {props.markers.map((bus,index) => (
+      <Marker key={index} position={bus.position} animation={google.maps.Animation.BOUNCE}
+        icon={{
+          url:"../Images/bus.png",
+          size: new google.maps.Size(71, 71),
+          origin: new google.maps.Point(0, 0),
+          anchor: new google.maps.Point(17, 17),
+          scaledSize: new google.maps.Size(25, 25)
+        }}
+        z-index={100}
+      />
+    ))}
+    {props.stops.map((stop,index) => (
+      <Marker key={index} position={stop.location} stopID={stop.StopID} name={stop.Name} routes={stop.Routes} animation={google.maps.Animation.DROP} opacity={0.8}
+        icon={{
+          url:"../Images/bus-stop.png",
+          size: new google.maps.Size(71, 71),
+          origin: new google.maps.Point(0, 0),
+          anchor: new google.maps.Point(17, 17),
+          scaledSize: new google.maps.Size(25, 25)
+        }}
+        z-index={2}
+        />
+      //   {this.state.position &&
+      //     <InfoWindow position={this.state.position}>
+      //      <Search />
+      //     </InfoWindow>
+      //   }
+      //   {props.isOpen && <InfoWindow key={index} onCloseClick={props.onToggleOpen}>
+      //     <Search />
+      //   </InfoWindow>}
+      // </Marker>
+    ))}
+    <Polyline path={props.path} options={{strokeColor:'black',strokeWeight: 2.5}} z-index={1} />
+    </GoogleMap>
 );
 export default MapRender;
 
