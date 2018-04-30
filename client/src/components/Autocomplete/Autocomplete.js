78a7076342d0c78ee3bb47a7f383138f66c42896
Angela Kressin <angelakressin@gmail.com>
2018-04-30 17:52:44 -0400

    implement working heart icon with animation for saving lines

--- client/src/components/Autocomplete/Autocomplete.js
+++ client/src/components/Autocomplete/Autocomplete.js
@@ -1,5 +1,4 @@
 import React from "react";
-import ReactDOM from 'react-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import AutoComplete from 'material-ui/AutoComplete';
 
