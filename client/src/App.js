9fe407b0f45b4347a4061760a00b91f72f3a9043
Angela Kressin <angelakressin@gmail.com>
2018-04-30 01:36:37 -0400

    removed TestSearch component

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import Home from "./pages";
 import Nav from "./components/Nav";
 import NavButtons from "./components/NavButtons";
-import TestSearch from "./components/TestSearch";
 import Form from "./components/Form";
 import FavNav from "./components/FavNav"
 import { Provider } from "react-alert";
